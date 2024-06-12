a8f0d002e1e0aac74de5d9714027c92cdece7f92
abdasg25 <abrehman.bscs22seecs@seecs.edu.pk>
2024-06-12 15:30:43 +0500

    Added .env to .gitignore

--- frontend/src/Pages/SignIn/signin.jsx
+++ frontend/src/Pages/SignIn/signin.jsx
@@ -3,27 +3,109 @@ import React, { useState } from 'react';
 import NavBar from '../../Components/NavBar/navBar';
 import Header from '../../Components/topHeader/Header';
 import Footer from '../../Components/Footer/Footer';
+import { loginSchema } from './loginSchema';
+import {useFormik} from 'formik';
+import Snackbar from '@mui/material/Snackbar';
 import './signin.css';
-
+const initialValues = {
+  email: "",
+  password: "",
+};
 const SignIn = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
+  const [message, setMessage] = useState(""); // Declare message and setMessage
+  const [displayAlert, setDisplayAlert] = useState(false); // Declare message and setMessage
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }));
-  };
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: loginSchema,
+      onSubmit: (values) => {
+        const apiUrl = `http://localhost:${process.env.FRONTEND_PORT}/Hospital/signin`;
+        const data = values;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form validation and submission logic here
-    console.log('Form submitted', formData);
-  };
+        const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        };
+
+        fetch(apiUrl, requestOptions)
+          .then(response => {
+            if (!response.ok) {
+              setDisplayAlert(true)
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            if (data.token) {
+              localStorage.setItem('token', data.token);
+              localStorage.setItem('role', data.role);
+              console.log(data);
+              setMessage("Login successful");
+              setDisplayAlert(true)
+              // alert("Login successful");
+              // localStorage.getItem('role') === "Admin"?
+              // window.location.href = '/Admin/Dashboard':
+              // window.location.href = '/home';
+            } else {
+              setMessage("Please check the Form/Internet.Signin Failed");
+              // alert("Please check the Form/Internet.Signin Failed");
+              setDisplayAlert(true)
+
+            }
+          })
+          .catch(error => {
+            // alert("Username password doest match");
+                   setMessage("Error signing in:",error);
+                   console.log(error);
+                   setDisplayAlert(true)
+
+          });
+      },
+    });
+
+
+
+
+
+
+
+
+
+
+    const [state, setState] = React.useState({
+      open: false,
+      vertical: 'top',
+      horizontal: 'center',
+    });
+    const { vertical, horizontal, open } = state;
+  
+    const handleClick = (newState) => () => {
+      setState({ ...newState, open: true });
+    };
+  
+    const handleClose = () => {
+      setDisplayAlert(false)
+      setState({ ...state, open: false });
+    };
+  
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value
+  //   }));
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Add form validation and submission logic here
+  //   console.log('Form submitted', formData);
+  // };
 
   const handleGoogleSignUp = () => {
     // Add Google sign-up logic here
@@ -34,6 +116,14 @@ const SignIn = () => {
     <>
       <Header />
       <NavBar />
+      {displayAlert?
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={true}
+        onClick={handleClose}
+        message= {message}
+        key={vertical + horizontal}
+      />:''}
       <div className="signup-container">
         <div className="image-container">
           <img src="/login.png" alt="Phone with Shopping Cart" />
@@ -45,19 +135,25 @@ const SignIn = () => {
             <input
               type="email"
               name="email"
-              placeholder="Email or Phone Number"
-              value={formData.email}
+              placeholder="Email "
+              value={values.email}
               onChange={handleChange}
               required
             />
+            {errors.email && touched.email ? (
+            <p className="form-error">{errors.username}</p>
+          ) : null}
             <input
               type="password"
               name="password"
               placeholder="Password"
-              value={formData.password}
+              value={values.password}
               onChange={handleChange}
               required
             />
+            {errors.password && touched.password ? (
+            <p className="form-error login-error">{errors.password}</p>
+          ) : null}
             <div classname='login'>
             <button type="submit" className="login-button">
               Log In
--- frontend/src/Pages/SignUp/signup.jsx
+++ frontend/src/Pages/SignUp/signup.jsx
@@ -4,10 +4,68 @@ import NavBar from '../../Components/NavBar/navBar';
 import Header from '../../Components/topHeader/Header';
 import Footer from '../../Components/Footer/Footer';
 import { useNavigate } from 'react-router-dom';
+import {useFormik} from 'formik';
+import { signUpSchema } from './signupSchema';
+import { signUp } from '../../Services api/signup';
 import './signup.css';
 
 const SignUp = () => {
+  const [message, setMessage] = useState(""); // Declare message and setMessage
+  const [displayAlert, setDisplayAlert] = useState(false); // Declare message and setMessage
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        // Define your initial form values here
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      },
+      validationSchema: signUpSchema,
+      onSubmit: async (values, { setSubmitting, setErrors }) => {
+        try {
+          const data = { username: values.name, email: values.email, password: values.password };
+          const response = await signUp(data);
+          console.log("Sign up successful!Please verify your Email.Check Inbox")
+          setMessage("Sign up successful!Please verify your Email.Check Inbox")
+          setDisplayAlert(true)
+        } catch (error) {
+          if (error && error.data && error.data.errors) {
+            setErrors(error.data.errors);
+            setMessage("Username Already Exist.Signup Failed")
+            setDisplayAlert(true)
+          } else {
+            // Handle generic error
+            setErrors({ general: 'An error occurred during sign up. Please try again.' });
+            setMessage("An error occurred during sign up. Please try again.")
+            setDisplayAlert(true)
+          }
+        } finally {
+          setSubmitting(false);
+        }
+      },
+
+    });
+    const [state, setState] = React.useState({
+      open: false,
+      vertical: 'top',
+      horizontal: 'center',
+    });
+    const { vertical, horizontal, open } = state;
+  
+    const handleClick = (newState) => () => {
+      setDisplayAlert(false)
+      setState({ ...newState, open: true });
+    };
+  
+    const handleClose = () => {
+      setState({ ...state, open: false });
+    };
+
     const [searchQuery, setSearchQuery] = useState('');
+
+
   const navigate = useNavigate();
 
   const handleSearchChange = (e) => {
@@ -26,26 +84,20 @@ const SignUp = () => {
     navigate(path);
   };
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword:''
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }));
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value
+  //   }));
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form validation and submission logic here
-    console.log('Form submitted', formData);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Add form validation and submission logic here
+  //   console.log('Form submitted', formData);
+  // };
 
   const handleGoogleSignUp = () => {
     // Add Google sign-up logic here
@@ -68,34 +120,48 @@ const SignUp = () => {
               type="text"
               name="name"
               placeholder="Name"
-              value={formData.name}
+              id='name'
+              value={values.name}
               onChange={handleChange}
+              onBlur={handleBlur}
               required
             />
             <input
               type="email"
               name="email"
-              placeholder="Email or Phone Number"
-              value={formData.email}
+              id='email'
+              placeholder="Email"
+              value={values.email}
               onChange={handleChange}
+              onBlur={handleBlur}
               required
             />
             <input
               type="password"
               name="password"
+              id='password'
               placeholder="Confirm Password"
-              value={formData.password}
+              value={values.password}
               onChange={handleChange}
+              onBlur={handleBlur}
               required
             />
+            {errors.password && touched.password ? (
+                <p className='form-error'>{errors.password}</p>
+              ) : null}
             <input
               type="password"
-              name="password"
+              name="confirmPassword"
+              id='confirmPassword'
               placeholder="Password"
-              value={formData.confirmPassword}
+              value={values.confirmPassword}
               onChange={handleChange}
+              onBlur={handleBlur}
               required
             />
+             {errors.confirmPassword && touched.confirmPassword ? (
+                <p className='form-error'>{errors.confirmPassword}</p>
+              ) : null}
             <button type="submit" className="create-account-button">
               Create Account
             </button>
